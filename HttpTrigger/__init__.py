10b66dbf4ff0e89c4a37710a5baaa887cc33132b
yuuka_miya <yuuka_miya@outlook.com>
2019-05-15 14:00:49 +0800

    prototype

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -2,23 +2,24 @@ import logging
 
 import azure.functions as func
 
+import pandas as pd
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    
+    df = pd.read_csv("https://github.com/yuuka-miya/ftrl-data/raw/master/processed_data/201903/origin_destination_train_201903_wholemonth_20190423123747.csv")
+        
+    df = df[["DAY_TYPE", "ORIGIN_PT_CODE", "DESTINATION_PT_CODE", "TOTAL_TRIPS"]]
+    
+    df = df[df['TOTAL_TRIPS'] !=0]
+    
+    
+    df = pd.concat([df[df["ORIGIN_PT_CODE"].str.contains("DT32")], df[df["DESTINATION_PT_CODE"].str.contains("DT32")]])
+    df = df.drop_duplicates()
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    df = df.sort_values(by='TOTAL_TRIPS', ascending=False)
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    df = df[df["DAY_TYPE"] == "WEEKDAY"]
+
+    return func.HttpResponse(df.to_json)
